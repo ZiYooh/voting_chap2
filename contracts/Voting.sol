8023b821def14a004242fa65bd79ee68b4588167
ZiYooh <natsucomi42@gmail.com>
2021-07-13 16:37:26 +0900

    초기 설정

---
+++ contracts/Voting.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.6;
+
+contract Voting{
+    bytes32[] public candidateList;
+    mapping (bytes32 => uint8) public votesReceived;
+
+    // Constructor to initialize candidates
+    constructor(bytes32[] memory candidateNames) public{
+        candidateList = candidateNames;
+    }
+
+    // Get count of votes for each candidates
+    function voteForCandidate(bytes32 candidate) public{
+        require(validCandidate(candidate));
+        votesReceived[candidate] += 1;
+    }
+
+    // Vote for candidates
+    function totalVotesFor(bytes32 candidate) view public returns(uint8){
+        require(validCandidate(candidate));
+        return votesReceived[candidate];
+    }
+    
+    // Check if candidate is valid
+    function validCandidate(bytes32 candidate) view public returns (bool){
+        for(uint i = 0; i < candidateList.length; i++){
+            if(candidateList[i] == candidate){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
